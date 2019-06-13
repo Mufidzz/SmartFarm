e48010e3f7c24ab2ff4d47fdc68e74696015a2f5
Mufidzz <dmzmzzz@users.noreply.github.com>
2019-06-13 16:53:24 +0700

    Folder Structure Repairing

---
+++ src/controller/MainMenuController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.scene.control.Label;
+
+public class MainMenuController {
+    public Label light;
+    public Label r;
+    public Label g;
+    public Label b;
+    public Label water;
+    public Label f;
+    public Label ph;
+    public Label cf;
+    public Label ppm;
+    public Label temp;
+    public Label rl;
+    public Label gl;
+    public Label bl;
+    public Label Quit;
+    public Label Status;
+}
