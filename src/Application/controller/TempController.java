26df498d2a15bdce7e657c921dc803363809f514
kiranakhns19 <49029418+kiranakhns19@users.noreply.github.com>
2019-06-18 21:13:34 +0700

    temp controller 1

--- src/Application/controller/TempController.java
+++ src/Application/controller/TempController.java
@@ -1,18 +1,17 @@
 package Application.controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainMenuController implements Initializable {
+public class TempController implements Initializable {
+
     @FXML
     private AnchorPane Parent;
 
@@ -25,14 +24,5 @@ public class MainMenuController implements Initializable {
         stage.initStyle(StageStyle.UNDECORATED);
 
         stage.show();
-    }
-
-    public void btnPlant_Clicked() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Application/views/Plant.fxml"));
-        fxmlLoader.load();
-    }
-    public void btnTemp_Clicked() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Application/views/Temperature.fxml"));
-        fxmlLoader.load();
     }
 }
