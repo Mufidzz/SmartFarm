7a4a98752983c600565848dd7d7fbe9635ec0932
rickyoktavian <rickymade06@gmail.com>
2019-06-18 21:51:47 +0700

    add: controller class

---
+++ src/Application/controller/FlowController.java
@@ -0,0 +1,28 @@
+package Application.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FlowController implements Initializable {
+
+    @FXML
+    private AnchorPane Parent;
+
+    Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        stage = new Stage();
+        stage.setScene(new Scene(Parent));
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.show();
+    }
+}
